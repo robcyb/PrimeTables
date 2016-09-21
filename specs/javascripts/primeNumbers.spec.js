09ee57aba8fd9c233b4624136396b3a803b31aae
Robert Simmons <robsimm@gmail.com>
2016-09-21 14:00:00 +0100

    Fixing order of assertion statements

--- specs/javascripts/primeNumbers.spec.js
+++ specs/javascripts/primeNumbers.spec.js
@@ -2,14 +2,14 @@
 
 describe("PrimeNumbers", function () {
   it("Returns true when a number is a prime number", function () {
-    expect(IsPrimeNumber(2).toBeTruthy());
+    expect(IsPrimeNumber(2)).toBeTruthy();
   });
 
   it("Returns false when a number is not a prime number", function () {
-    expect(IsPrimeNumber(4).toBeFalsy());
+    expect(IsPrimeNumber(4)).toBeFalsy();
   });
 
   it("Returns false when the input is less than 1", function () {
-    expect(IsPrimeNumber(0).toBeFalsy());
+    expect(IsPrimeNumber(0)).toBeFalsy();
   });
 })
