0da756ca39060f53eba42fe637e6ba72d59a7e5a
Robert Simmons <robsimm@gmail.com>
2016-09-21 14:27:07 +0100

    Adding additional assertions - Ensuring primes are calculating for multiple sized inputs

--- specs/javascripts/primeNumbers.spec.js
+++ specs/javascripts/primeNumbers.spec.js
@@ -4,10 +4,14 @@ describe("PrimeNumbers", function () {
   describe("PrimeNumbers calculations are working", function () {
     it("Returns true when a number is a prime number", function () {
       expect(IsPrimeNumber(2)).toBeTruthy();
+      expect(IsPrimeNumber(11)).toBeTruthy();
+      expect(IsPrimeNumber(101)).toBeTruthy();
     });
 
     it("Returns false when a number is not a prime number", function () {
       expect(IsPrimeNumber(4)).toBeFalsy();
+      expect(IsPrimeNumber(12)).toBeFalsy();
+      expect(IsPrimeNumber(100)).toBeFalsy();
     });
 
     it("Returns false when the input is less than 1", function () {
--- src/javascripts/primeNumbers.js
+++ src/javascripts/primeNumbers.js
@@ -1,12 +1,12 @@
 function IsPrimeNumber(number) {
 
   if (number <= 0) {
-    return false;
+    return false; // only return when number is a positive number - as per specification.
   }
 
-  var index, limit = Math.sqrt(number);
+  var index, limit = Math.sqrt(number); // whole numbers only - as per specification.
 
-  for (index = 2; index <= limit; index += 1) {
+  for (index = 2; index <= limit; index++) {
     if (number % index === 0) {
       return false;
     }
