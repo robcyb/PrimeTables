68ccdb5df8fdd25ac2daa7ec94d1801378ba27df
Robert Simmons <robsimm@gmail.com>
2016-09-21 14:03:18 +0100

    Get prime numbers true/false tests green lighting - Implemented core logic for check if a number is prime - Return false if number is not prime

--- src/javascripts/primeNumbers.js
+++ src/javascripts/primeNumbers.js
@@ -2,4 +2,14 @@ function IsPrimeNumber(number) {
   if (number <= 0) {
     return false;
   }
+
+  var index, limit = Math.sqrt(number);
+
+  for (index = 2; index <= limit; index += 1) {
+    if (number % index === 0) {
+      return false;
+    }
+  }
+  return true;
+
 }
