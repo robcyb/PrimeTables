f63ff3ef86e26220efdd9072a4cc27857087c138
Robert Simmons <robsimm@gmail.com>
2016-09-21 13:57:39 +0100

    Initial failing tests for isPrimeNumber function

--- specs/javascripts/primeNumbers.spec.js
+++ specs/javascripts/primeNumbers.spec.js
@@ -1,7 +1,15 @@
 "use strict";
 
 describe("PrimeNumbers", function () {
-  it("Is available and defined", function () {
-    expect(primeNumbers).toBeDefined();
-  })
+  it("Returns true when a number is a prime number", function () {
+    expect(IsPrimeNumber(2).toBeTruthy());
+  });
+
+  it("Returns false when a number is not a prime number", function () {
+    expect(IsPrimeNumber(4).toBeFalsy());
+  });
+
+  it("Returns false when the input is less than 1", function () {
+    expect(IsPrimeNumber(0).toBeFalsy());
+  });
 })
